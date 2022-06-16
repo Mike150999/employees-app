1cd37272813c91112cd27ed674e07c9fcf594277
Mike <myanushkov@gmail.com>
2022-06-16 19:07:36 +0300

    first commit

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,12 @@
+import './app-info.css';
+const AppInfo = ({ employees, increased }) => {
+	return (
+		<div className="app-info">
+			<h1>Облік працівників в компанії</h1>
+			<h2>Загальна кількість працівників: {employees}</h2>
+			<h2>Премію отримають: {increased}</h2>
+		</div>
+	)
+}
+
+export default AppInfo;
